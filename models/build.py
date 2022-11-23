f43bf1904dfc1e3f78499d084dc5569e19dd4653
Md Shamim Hussain <snirjhar@gmail.com>
2022-11-22 22:11:46 -0500

    use swin v1 instead of v2

--- models/build.py
+++ models/build.py
@@ -63,22 +63,23 @@ def build_model(config, is_pretrain=False):
                                   pretrained_window_sizes=config.MODEL.SWINV2.PRETRAINED_WINDOW_SIZES)
     elif model_type == 'swing':
         model = SwinTransformerG(img_size=config.DATA.IMG_SIZE,
-                                 patch_size=config.MODEL.SWINV2.PATCH_SIZE,
-                                 in_chans=config.MODEL.SWINV2.IN_CHANS,
+                                 patch_size=config.MODEL.SWIN.PATCH_SIZE,
+                                 in_chans=config.MODEL.SWIN.IN_CHANS,
                                  num_classes=config.MODEL.NUM_CLASSES,
-                                 embed_dim=config.MODEL.SWINV2.EMBED_DIM,
-                                 depths=config.MODEL.SWINV2.DEPTHS,
-                                 num_heads=config.MODEL.SWINV2.NUM_HEADS,
-                                 window_size=config.MODEL.SWINV2.WINDOW_SIZE,
-                                 mlp_ratio=config.MODEL.SWINV2.MLP_RATIO,
-                                 qkv_bias=config.MODEL.SWINV2.QKV_BIAS,
+                                 embed_dim=config.MODEL.SWIN.EMBED_DIM,
+                                 depths=config.MODEL.SWIN.DEPTHS,
+                                 num_heads=config.MODEL.SWIN.NUM_HEADS,
+                                 window_size=config.MODEL.SWIN.WINDOW_SIZE,
+                                 mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
+                                 qkv_bias=config.MODEL.SWIN.QKV_BIAS,
+                                 qk_scale=config.MODEL.SWIN.QK_SCALE,
                                  drop_rate=config.MODEL.DROP_RATE,
                                  drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                                 ape=config.MODEL.SWINV2.APE,
-                                 patch_norm=config.MODEL.SWINV2.PATCH_NORM,
+                                 ape=config.MODEL.SWIN.APE,
+                                 norm_layer=layernorm,
+                                 patch_norm=config.MODEL.SWIN.PATCH_NORM,
                                  use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                                 pretrained_window_sizes=config.MODEL.SWINV2.PRETRAINED_WINDOW_SIZES)
-                                
+                                 fused_window_process=config.FUSED_WINDOW_PROCESS)
     else:
         raise NotImplementedError(f"Unkown model: {model_type}")
 
